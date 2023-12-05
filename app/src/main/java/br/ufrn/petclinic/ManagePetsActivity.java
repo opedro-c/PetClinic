2929d941bdcffe884a25cc6f79b57cc93a3f22b2
Pedro Costa <pedroc_aragao@outlook.com>
2023-12-05 20:21:26 -0300

    fix navigation

--- app/src/main/java/br/ufrn/petclinic/ManagePetsActivity.java
+++ app/src/main/java/br/ufrn/petclinic/ManagePetsActivity.java
@@ -1,14 +1,14 @@
 package br.ufrn.petclinic;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class ManagePetsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_manage_pets);
     }
 }
